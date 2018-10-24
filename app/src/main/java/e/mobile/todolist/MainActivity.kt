5a78cefefaec222de52edbca3ae1039bb322017e
juumilani <juumilani@hotmail.com>
2018-10-24 15:34:33 -0300

    Recycler view e botão adicionados

--- app/src/main/java/e/mobile/todolist/MainActivity.kt
+++ app/src/main/java/e/mobile/todolist/MainActivity.kt
@@ -2,11 +2,16 @@ package e.mobile.todolist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        }
+
     }
-}
+
