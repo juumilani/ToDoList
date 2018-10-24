e79a9c37fe3f7fd587b4f6d19daf927cb0641259
Cassiano Maia <cassmala@gmail.com>
2018-10-24 15:30:05 -0300

    Adcionando algumas funcionalidades a função de cadastro

--- app/src/main/java/e/mobile/todolist/cadastroToDoActivity.kt
+++ app/src/main/java/e/mobile/todolist/cadastroToDoActivity.kt
@@ -2,11 +2,27 @@ package e.mobile.todolist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro_to_do.*
 
 class cadastroToDoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_to_do)
+
+        btnSave.setOnClickListener() {
+            salvaToDo()
+        }
     }
+
+    private fun salvaToDo() {
+        if(edtToDo.text.isEmpty()){
+            edtToDo.requestFocus()
+            edtToDo.setError(getString(R.string.vazio))
+            return
+        }
+        //TODO: Implement save code
+    }
+
+
 }
