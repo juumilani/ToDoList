18814986847f88881b36975e0791f36e8995a37e
Cassiano Maia <cassmala@gmail.com>
2018-10-24 15:09:42 -0300

    Criação da activity de cadastro

---
+++ app/src/main/java/e/mobile/todolist/cadastroToDoActivity.kt
@@ -0,0 +1,12 @@
+package e.mobile.todolist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class cadastroToDoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_to_do)
+    }
+}
